bd5717bba634fd62a7b2ed1f8b8f95f2757ab8b2
akhilamuralikrishna <149869327+akhilamuralikrishna@users.noreply.github.com>
2025-11-10 11:58:22 +0530

    58623

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import *
+from .viwes import MenuCategoryListView
 
-urlpatterns = [
-    
+urlpatterns=[
+    path('menu-categories/',
+    MenuCategoryListView.as_view(),
+    name='menu-categories'),
 ]
