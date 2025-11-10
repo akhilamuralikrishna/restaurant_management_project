83dc94c7056f4960be041dcc96ca567a8ca63456
akhilamuralikrishna <149869327+akhilamuralikrishna@users.noreply.github.com>
2025-11-10 11:29:38 +0530

    58622

--- orders/views.py
+++ orders/views.py
@@ -7,19 +7,19 @@ from rest_framework import status
 from .models import Coupon
 
 class CouponValidationView(APIview):
-    def post(self,request)
-    code=request.data.get('code')
+    def post(self,request):
+      code=request.data.get('code')
 
     if not code:
-        return Response({'error':'coupon code is rquired'},status=status.HTTP_400_BAD_REQUEST)
+        return Response({'error':'Coupon code is required'},status=status.HTTP_400_BAD_REQUEST)
     try:
-        coupon=Coupon.Objects.get(code=code)
+        coupon=Coupon.objects.get(code=code)
     except Coupon.DoesNotExist:
         return Response({'Error':'Invalid Coupon Code'},status=status.HTTP_404_NOT_REQUEST)
         if coupon.is_valid():
             return Response({
                 'success':True,
-                'message':'couple is valid',
+                'message':'Coupon is valid',
                 'discount':float(coupon.discount)
             },status=status.HTTP_200_OK)
         return Response({'error':'Coupon expired or inactive'},status=status.HTTP_400_BAD_REQUEST)            
