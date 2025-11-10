3de0cbc9008343cdb1c609b9bfe4f0f53d0b64b1
akhilamuralikrishna <149869327+akhilamuralikrishna@users.noreply.github.com>
2025-11-10 10:44:09 +0530

    58621

--- orders/models.py
+++ orders/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.db.models.signals import post_migrate
+from django.dispatch import receiver
 
 # Create your models here.
 class OrderStatus(models.Model):
@@ -6,8 +8,9 @@ class OrderStatus(models.Model):
 
     def __str__(self):
         return self.name
-class Order(models.Model):
-    status=models.ForeignKey(OrderStatus,on_delete=models.SET_NULL,null=True)
-    
-    def __str__(self):
-        return f'order{self.id}        
+@receiver(post_migrate)
+def add_default_statuses(sender,**kwargs):
+    if sender.name=='orders':
+        statuses=['PENDING','PROCESSING','COMPLETED','CANCELLED']
+        for status in statuses:
+            OrderStatus.objects.get__or__create(name=status)
