6bc2c57a6038ee661d920e372b6bf517047ac802
akhilamuralikrishna <149869327+akhilamuralikrishna@users.noreply.github.com>
2025-11-17 10:11:32 +0530

    60607

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import FeaturedMenuItemsViews
+from .views import ItemView ,FeaturedMenuItemsView
 
 urlpatterns = [
     path('items/', ItemView.as_view(), name='item-list'),
+    path('featured_items/',FeaturedMenuItemsView.as_view(),name='featured_items'),
 ]
