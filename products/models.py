caefda880c76fc2858c6864b79815f0add23b9f3
akhilamuralikrishna <149869327+akhilamuralikrishna@users.noreply.github.com>
2025-11-17 09:57:03 +0530

    60607

--- products/models.py
+++ products/models.py
@@ -5,6 +5,7 @@ class Item(models.Model):
     item_name = models.CharField(max_length=150)
     item_price = models.DecimalField(max_digits=10, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
+    is_featured=models.BooleanField(default=False)
 
     def __str__(self):
         return str(self.item_name)
