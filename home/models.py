4e597bf36b96f5c09778874d8aa10ff5269af165
akhilamuralikrishna <149869327+akhilamuralikrishna@users.noreply.github.com>
2025-11-19 11:05:26 +0530

    60844

--- home/models.py
+++ home/models.py
@@ -5,5 +5,17 @@ class MenuCategory(models.Model):
     def __str__(self):
         return self.name
 
-# Create your models here.
+class DailySpecial(models.Model):
+    name=models.CharField(max_length=100)
+    description=models.TextField(blank=True)
+    price=models.DecimalField(max_digits=6,decimal_places=2)
+    def __str__(self):
+        return self.name
+    @staticmethod 
+    def get_random_special():
+        specials=DailySpecial.objects.all()
+        
+        if not specials.exists():
+            return None
+        return specials.order_by('?').first()      #Create your models here.
 
