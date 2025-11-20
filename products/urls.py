fe2ca87ed9c4512bfa02955bdf5d9d61f2191608
akhilamuralikrishna <149869327+akhilamuralikrishna@users.noreply.github.com>
2025-11-20 14:46:21 +0530

    60846

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .views import ItemView ,FeaturedMenuItemsView
+from .views import SearchItemAPIView
 
 urlpatterns = [
     path('items/', ItemView.as_view(), name='item-list'),
     path('featured_items/',FeaturedMenuItemsView.as_view(),name='featured_items'),
+    path('search-items/',SearchItemAPIView.as_viwe())
 ]
