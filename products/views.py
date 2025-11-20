294648ef26b5430a91ae617740f311514096580b
akhilamuralikrishna <149869327+akhilamuralikrishna@users.noreply.github.com>
2025-11-20 14:44:11 +0530

    60846

--- products/views.py
+++ products/views.py
@@ -5,6 +5,7 @@ from rest_framework import status
 
 from .models import Item
 from .serializers import ItemSerializer
+from django.core.paginator import import Paginator
 
 '''
 NOTE: Conside this as a reference and follow this same coding structure or format to work on you tasks
@@ -28,4 +29,16 @@ class FeaturedMenuItemsView(APIView):
     def get(self,request):
         items=MenuItem.objects.filter(is_featured=True)
         serializer=ItemSerializer(items,many=True)
-        return Response(serializer.data,status=status.HTTP_200_OK)        
+        return Response(serializer.data,status=status.HTTP_200_OK)
+class SearchItemAPIView(APIView):
+    def get(self,request):
+        query=request.GET.get('q','')
+        items=Item.objects.filter(name__icontains=query)
+        paginator=Paginator(items,10)
+        page_number=request.GET.get('page',1)
+        page_obj=paginator.get_page(page_number)
+        serializer=ItemSerializer(page_obj,many=True)
+        return Response({'count':paginator.count,
+        'total_pages':paginator.num_pages,
+        'current_page':page_obj.number,
+        'results':serializer.data},status=status.HTTP_200_OK)              
