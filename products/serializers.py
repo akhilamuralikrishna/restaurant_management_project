9a273305b32a10bd8aee2b2419d9d8b1d5245221
akhilamuralikrishna <149869327+akhilamuralikrishna@users.noreply.github.com>
2025-11-27 10:27:09 +0530

    61251

--- products/serializers.py
+++ products/serializers.py
@@ -1,7 +1,18 @@
 from rest_framework import serializers
-from .models import Item
+from .models import Item, Ingredient, MenuItem
 
 class ItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = Item
         fields = '__all__'
+class IngredientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ingredient  
+        fields = ['id','name']
+class MenuItemSerializer(serializers.ModelSerializer):
+    ingredients = IngredientSerializer(many = True, read_only = True)
+    class Meta:
+        model = MenuItem
+        fields = ['id','name','ingredients']       
+
+
