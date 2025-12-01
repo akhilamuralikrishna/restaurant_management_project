70f85a88c34bba2ef92cda048b9a648ccd5c41be
akhilamuralikrishna <149869327+akhilamuralikrishna@users.noreply.github.com>
2025-12-01 19:29:55 +0530

    61254

--- home/views.py
+++ home/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-from rest_framework.views import API
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from products.models import MenuItem
 from products.serializers import IngredientSerializer
+from .serializers import MenuItemSerializer
+
 class MenuItemIngredientsView(APIView):
     def get(self,request,menu_item_id):
         try:
@@ -11,7 +13,19 @@ class MenuItemIngredientsView(APIView):
                 return Response({'error':'menu item not found'},status=status.HTTP_404_NOT_FOUND)
             ingredients=menu_item.ingredients.all()
             serializer = IngredientSerializer(ingredients,many=True)
-            return Response(serializer.data)   
+            return Response(serializer.data)
+
+class UpdateMenuItem(APIView):
+    def put(self, request, menu_item_id):
+        try:
+            menu_item = MenuItem.objects.get(id=menu_item_id)
+        except MenuItem.DoesNotExist:
+            return Response({'error':'Menu item not found'},status=status.HTTP_404_NOT_FOUND)
+        serializer = MenuItemSerializer(menu_item, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_200_OK)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)                          
 
 
 
