520dd63b433f406cac989a333cf701510ba123ed
akhilamuralikrishna <149869327+akhilamuralikrishna@users.noreply.github.com>
2025-12-01 19:11:47 +0530

    61254

--- home/models.py
+++ home/models.py
@@ -26,5 +26,14 @@ class DailySpecial(models.Model):
         
         if not specials.exists():
             return None
-        return specials.order_by('?').first()      #Create your models here.
+        return specials.order_by('?').first()
+
+class MenuItem(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=6,decimal_places=2)
+    available = models.BooleanField(default=True)
+    
+    def __str__(self):
+        return self.name              #Create your models here.
 
