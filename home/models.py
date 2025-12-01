2b7a91b661419a22bdf09ae9a070a08f803ea711
akhilamuralikrishna <149869327+akhilamuralikrishna@users.noreply.github.com>
2025-12-01 19:04:20 +0530

    61253

--- home/models.py
+++ home/models.py
@@ -1,14 +1,23 @@
 from django.db import models
+from datetime import date
 
 class MenuCategory(models.Model):
     name=models.CharField(max_length=100,unique=True)
     def __str__(self):
         return self.name
 
+class DailySpecialManager(models.Manager):
+    def upcoming(self):
+        today = date.today()
+super().get_queryset().filter(special_date__get=today) 
+
+
 class DailySpecial(models.Model):
     name=models.CharField(max_length=100)
     description=models.TextField(blank=True)
     price=models.DecimalField(max_digits=6,decimal_places=2)
+    special_date=models.DateField()
+    objects=DailySpecialManager()
     def __str__(self):
         return self.name
     @staticmethod 
