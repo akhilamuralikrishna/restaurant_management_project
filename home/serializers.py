8976f179994a1f1c9672caa66641cb059c05d853
akhilamuralikrishna <149869327+akhilamuralikrishna@users.noreply.github.com>
2025-12-01 19:16:50 +0530

    61254

--- home/serializers.py
+++ home/serializers.py
@@ -1,7 +1,16 @@
 from rest_frmework import serializers
 from .models import MenuCategory
+from .models import MenuItem
 
 class MenuCategorySerializer(serializers.ModelSerializer):
     class Meta:
         model=MenuCategory
         fields=['id','name']
+class MenuItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = ['id','name','description','price','available']
+
+    def validate_price(self,value):
+        if value <= 0:
+            raise serializer.ValidationError('Price must be positive.')
+        return value               
