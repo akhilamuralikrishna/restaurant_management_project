dd628c2ea51702ddf5d2842a4efb0215accbfd48
akhilamuralikrishna <149869327+akhilamuralikrishna@users.noreply.github.com>
2025-12-04 12:59:07 +0530

    61598

--- products/serializers.py
+++ products/serializers.py
@@ -11,8 +11,9 @@ class IngredientSerializer(serializers.ModelSerializer):
         fields = ['id','name']
 class MenuItemSerializer(serializers.ModelSerializer):
     ingredients = IngredientSerializer(many = True, read_only = True)
+    Category = serializers.CharField(source = 'category.name',read_only = True)
     class Meta:
         model = MenuItem
-        fields = ['id','name','ingredients']       
+        fields = ['id','name','category','ingredients']       
 
 
