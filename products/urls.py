59a39f3c38d1afe78848ca0497d276541a2da3a2
akhilamuralikrishna <149869327+akhilamuralikrishna@users.noreply.github.com>
2025-12-04 13:15:45 +0530

    61598

--- products/urls.py
+++ products/urls.py
@@ -5,5 +5,6 @@ from .views import SearchItemAPIView
 urlpatterns = [
     path('items/', ItemView.as_view(), name='item-list'),
     path('featured_items/',FeaturedMenuItemsView.as_view(),name='featured_items'),
-    path('search-items/',SearchItemAPIView.as_viwe())
+    path('search-items/',SearchItemAPIView.as_view()),
+    path('menu-items-by-category/',MenuItemByCategory.as_view())
 ]
