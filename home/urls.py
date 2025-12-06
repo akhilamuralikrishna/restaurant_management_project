06a99df981f12bbe76fb1a3ff60119dc1a139cc5
akhilamuralikrishna <149869327+akhilamuralikrishna@users.noreply.github.com>
2025-12-06 11:20:25 +0530

    61599

--- home/urls.py
+++ home/urls.py
@@ -2,11 +2,13 @@ from django.urls import path
 from .viwes import MenuCategoryListView
 from home.views import MenuItemIngredientsView
 from .views import UpdateMenuItem
+from .views import TableDetailAPIView
 
 urlpatterns=[
     path('menu-categories/',
     MenuCategoryListView.as_view(),
     name='menu-categories'),
     path('menu-items/<int:menu_item_id>/ingredients/',MenuItemIngredientsView.as_view()),
-    path('menu/update/<int:menu_item_id>/')
+    path('menu/update/<int:menu_item_id>/'),
+    path('api-tables/<int:pk>/',TableDetailAPIView.as_view(), name ='table-detail') 
 ]
