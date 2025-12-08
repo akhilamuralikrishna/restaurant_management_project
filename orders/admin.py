a1646b603b9204bf538c0ddc1404d9eef24edbed
akhilamuralikrishna <149869327+akhilamuralikrishna@users.noreply.github.com>
2025-12-08 14:43:03 +0530

    61601

--- orders/admin.py
+++ orders/admin.py
@@ -3,10 +3,12 @@ from .models import Order
 
 # Register your models here.
 def mark_as_processed(modeladmin, request, queryset):
-    queryset.update(status='processed')
+    completed_status,created = OrderStatus.objects.get_or_create(name='COMPLETED')
+    queryset.updated(status=completed_status)
+
 mark_as_processed.short_description = 'Mark Selected orders as a Processed'
 
 class OrderAdmin(admin.Modeladmin):
     list_display = ('id','user','status','created_at')
-    actions = [mark_as_processed]
+    actions = [mark_as_completed]
 admin.site.register(Order, OrderAdmin)    
