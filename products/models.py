d258dcb986bb679fcfbe117caf34cc0bc3796436
akhilamuralikrishna <149869327+akhilamuralikrishna@users.noreply.github.com>
2025-12-08 12:41:11 +0530

    61600

--- products/models.py
+++ products/models.py
@@ -1,6 +1,11 @@
 from django.db import models
+from django.db.models import Count
 
 # Create your models here.
+class MenuItemManager(models.Manager):
+    def top_selling(self, num_items=5):
+        return self.annotate(total_orders=Count('orderitem')).order_by('-total-orders')[:num_items]
+
 class Item(models.Model):
     item_name = models.CharField(max_length=150)
     item_price = models.DecimalField(max_digits=10, decimal_places=2)
@@ -29,6 +34,7 @@ class MenuItem(models.Model):
     name = models.CharField(max_length = 100)
     ingredients = models.ManyToManyField(Ingredient,related_name='menu_items')
     category = models.ForeignKey(Category,on_delete=models.CASCADE,related_name='menu_item')
+    objects = MenuItemManager()
 
     def __str__(self):
         return self.name
